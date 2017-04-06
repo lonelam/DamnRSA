bc5be227586ed5028f0b6d850605b2f28a6528a4
Lonelam <laizenan@gmail.com>
2017-04-06 21:15:04 +0800

    completed homework

---
+++ DamnRSA/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace DamnRSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PubK = new System.Windows.Forms.Label();
+            this.PrivK = new System.Windows.Forms.Label();
+            this.pub_box = new System.Windows.Forms.TextBox();
+            this.pv_box = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PubK
+            // 
+            this.PubK.AutoSize = true;
+            this.PubK.Location = new System.Drawing.Point(12, 9);
+            this.PubK.Name = "PubK";
+            this.PubK.Size = new System.Drawing.Size(79, 15);
+            this.PubK.TabIndex = 0;
+            this.PubK.Text = "PublicKey";
+            // 
+            // PrivK
+            // 
+            this.PrivK.AutoSize = true;
+            this.PrivK.Location = new System.Drawing.Point(417, 9);
+            this.PrivK.Name = "PrivK";
+            this.PrivK.Size = new System.Drawing.Size(87, 15);
+            this.PrivK.TabIndex = 1;
+            this.PrivK.Text = "PriviteKey";
+            // 
+            // pub_box
+            // 
+            this.pub_box.Location = new System.Drawing.Point(12, 27);
+            this.pub_box.Multiline = true;
+            this.pub_box.Name = "pub_box";
+            this.pub_box.Size = new System.Drawing.Size(402, 226);
+            this.pub_box.TabIndex = 2;
+            // 
+            // pv_box
+            // 
+            this.pv_box.Location = new System.Drawing.Point(420, 27);
+            this.pv_box.Multiline = true;
+            this.pv_box.Name = "pv_box";
+            this.pv_box.Size = new System.Drawing.Size(376, 637);
+            this.pv_box.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "GenerateKeyPair";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "message";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 325);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(399, 28);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Digest(Sha1)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 385);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(399, 28);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 444);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(399, 85);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 422);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Signature";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 532);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 15);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Resolved Signature";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 550);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(399, 85);
+            this.textBox4.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 676);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pv_box);
+            this.Controls.Add(this.pub_box);
+            this.Controls.Add(this.PrivK);
+            this.Controls.Add(this.PubK);
+            this.Name = "Form1";
+            this.Text = "DamnRSA——计算机科学进展研讨";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PubK;
+        private System.Windows.Forms.Label PrivK;
+        private System.Windows.Forms.TextBox pub_box;
+        private System.Windows.Forms.TextBox pv_box;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ DamnRSA/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using Org.BouncyCastle;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Math;
+using System.IO;
+
+namespace DamnRSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private RSACryptoServiceProvider rsap;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rsap = new RSACryptoServiceProvider();
+            var p = rsap.ExportParameters(false);
+
+            var pubkeyout = new Org.BouncyCastle.Crypto.Parameters.RsaKeyParameters(false, new BigInteger(1, p.Modulus), new BigInteger(1, p.Exponent));
+
+            var pubbuilder = new StringBuilder();
+            var pubwriter = new System.IO.StringWriter(pubbuilder);
+            var pw = new Org.BouncyCastle.OpenSsl.PemWriter(pubwriter);
+            pw.WriteObject(pubkeyout);
+            pub_box.Text = pubbuilder.ToString();
+            var pv = rsap.ExportParameters(true);
+            var privkeyout = new Org.BouncyCastle.Crypto.Parameters.RsaPrivateCrtKeyParameters(
+                new BigInteger(1, pv.Modulus), new BigInteger(1,pv.Exponent), new BigInteger(1,pv.D), new BigInteger(1, pv.P), new BigInteger(1, pv.Q),
+                new BigInteger(1,pv.DP), new BigInteger(1,pv.DQ), new BigInteger(1, pv.InverseQ)
+                );
+            var privbuilder = new StringBuilder();
+            var privwriter = new System.IO.StringWriter(privbuilder);
+            var pvw = new Org.BouncyCastle.OpenSsl.PemWriter(privwriter);
+            pvw.WriteObject(privkeyout);
+            pv_box.Text = privbuilder.ToString();
+        }
+
+        private RSACryptoServiceProvider CreateRsaProviderFromPrivateKey(string privateKey)
+        {
+            var privateKeyBits = System.Convert.FromBase64String(privateKey);
+
+            var RSA = new RSACryptoServiceProvider();
+            var RSAparams = new RSAParameters();
+
+            using (BinaryReader binr = new BinaryReader(new MemoryStream(privateKeyBits)))
+            {
+                byte bt = 0;
+                ushort twobytes = 0;
+                twobytes = binr.ReadUInt16();
+                if (twobytes == 0x8130)
+                    binr.ReadByte();
+                else if (twobytes == 0x8230)
+                    binr.ReadInt16();
+                else
+                    throw new Exception("Unexpected value read binr.ReadUInt16()");
+
+                twobytes = binr.ReadUInt16();
+                if (twobytes != 0x0102)
+                    throw new Exception("Unexpected version");
+
+                bt = binr.ReadByte();
+                if (bt != 0x00)
+                    throw new Exception("Unexpected value read binr.ReadByte()");
+
+                RSAparams.Modulus = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.Exponent = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.D = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.P = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.Q = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.DP = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.DQ = binr.ReadBytes(GetIntegerSize(binr));
+                RSAparams.InverseQ = binr.ReadBytes(GetIntegerSize(binr));
+            }
+
+            RSA.ImportParameters(RSAparams);
+            return RSA;
+        }
+
+
+        private int GetIntegerSize(BinaryReader binr)
+        {
+            byte bt = 0;
+            byte lowbyte = 0x00;
+            byte highbyte = 0x00;
+            int count = 0;
+            bt = binr.ReadByte();
+            if (bt != 0x02)
+                return 0;
+            bt = binr.ReadByte();
+
+            if (bt == 0x81)
+                count = binr.ReadByte();
+            else
+                if (bt == 0x82)
+            {
+                highbyte = binr.ReadByte();
+                lowbyte = binr.ReadByte();
+                byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };
+                count = BitConverter.ToInt32(modint, 0);
+            }
+            else
+            {
+                count = bt;
+            }
+
+            while (binr.ReadByte() == 0x00)
+            {
+                count -= 1;
+            }
+            binr.BaseStream.Seek(-1, SeekOrigin.Current);
+            return count;
+        }
+
+        private RSACryptoServiceProvider CreateRsaProviderFromPublicKey(string publicKeyString)
+        {
+            // encoded OID sequence for  PKCS #1 rsaEncryption szOID_RSA_RSA = "1.2.840.113549.1.1.1"
+            byte[] SeqOID = { 0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00 };
+            byte[] x509key;
+            byte[] seq = new byte[15];
+            int x509size;
+
+            x509key = Convert.FromBase64String(publicKeyString);
+            x509size = x509key.Length;
+
+            // ---------  Set up stream to read the asn.1 encoded SubjectPublicKeyInfo blob  ------
+            using (MemoryStream mem = new MemoryStream(x509key))
+            {
+                using (BinaryReader binr = new BinaryReader(mem))  //wrap Memory Stream with BinaryReader for easy reading
+                {
+                    byte bt = 0;
+                    ushort twobytes = 0;
+
+                    twobytes = binr.ReadUInt16();
+                    if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
+                        binr.ReadByte();    //advance 1 byte
+                    else if (twobytes == 0x8230)
+                        binr.ReadInt16();   //advance 2 bytes
+                    else
+                        return null;
+
+                    seq = binr.ReadBytes(15);       //read the Sequence OID
+                    if (!CompareBytearrays(seq, SeqOID))    //make sure Sequence for OID is correct
+                        return null;
+
+                    twobytes = binr.ReadUInt16();
+                    if (twobytes == 0x8103) //data read as little endian order (actual data order for Bit String is 03 81)
+                        binr.ReadByte();    //advance 1 byte
+                    else if (twobytes == 0x8203)
+                        binr.ReadInt16();   //advance 2 bytes
+                    else
+                        return null;
+
+                    bt = binr.ReadByte();
+                    if (bt != 0x00)     //expect null byte next
+                        return null;
+
+                    twobytes = binr.ReadUInt16();
+                    if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
+                        binr.ReadByte();    //advance 1 byte
+                    else if (twobytes == 0x8230)
+                        binr.ReadInt16();   //advance 2 bytes
+                    else
+                        return null;
+
+                    twobytes = binr.ReadUInt16();
+                    byte lowbyte = 0x00;
+                    byte highbyte = 0x00;
+
+                    if (twobytes == 0x8102) //data read as little endian order (actual data order for Integer is 02 81)
+                        lowbyte = binr.ReadByte();  // read next bytes which is bytes in modulus
+                    else if (twobytes == 0x8202)
+                    {
+                        highbyte = binr.ReadByte(); //advance 2 bytes
+                        lowbyte = binr.ReadByte();
+                    }
+                    else
+                        return null;
+                    byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };   //reverse byte order since asn.1 key uses big endian order
+                    int modsize = BitConverter.ToInt32(modint, 0);
+
+                    int firstbyte = binr.PeekChar();
+                    if (firstbyte == 0x00)
+                    {   //if first byte (highest order) of modulus is zero, don't include it
+                        binr.ReadByte();    //skip this null byte
+                        modsize -= 1;   //reduce modulus buffer size by 1
+                    }
+
+                    byte[] modulus = binr.ReadBytes(modsize);   //read the modulus bytes
+
+                    if (binr.ReadByte() != 0x02)            //expect an Integer for the exponent data
+                        return null;
+                    int expbytes = (int)binr.ReadByte();        // should only need one byte for actual exponent data (for all useful values)
+                    byte[] exponent = binr.ReadBytes(expbytes);
+
+                    // ------- create RSACryptoServiceProvider instance and initialize with public key -----
+                    RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+                    RSAParameters RSAKeyInfo = new RSAParameters();
+                    RSAKeyInfo.Modulus = modulus;
+                    RSAKeyInfo.Exponent = exponent;
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    return RSA;
+                }
+
+            }
+        }
+        private bool CompareBytearrays(byte[] a, byte[] b)
+        {
+            if (a.Length != b.Length)
+                return false;
+            int i = 0;
+            foreach (byte c in a)
+            {
+                if (c != b[i])
+                    return false;
+                i++;
+            }
+            return true;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            var SHAalg = new SHA1CryptoServiceProvider();
+            var bmsg = Encoding.UTF8.GetBytes(textBox1.Text);
+            var shamsg = SHAalg.ComputeHash(bmsg);
+            var bsig = rsap.SignHash(shamsg,"SHA1");
+            StringBuilder sb = new StringBuilder();
+
+            for (int i= 0; i < shamsg.Length; i++)
+            {
+                sb.Append(shamsg[i].ToString("X2"));
+            }
+            var sig = Convert.ToBase64String(bsig);
+            textBox2.Text = sb.ToString();
+            textBox3.Text = sig;
+            button1.Text = rsap.VerifyData(bmsg, "SHA1", bsig).ToString();
+            RSACryptoServiceProvider rsa2 = new RSACryptoServiceProvider();
+            rsa2.ImportParameters(rsap.ExportParameters(false));
+            textBox4.Text = sb.ToString();
+        }
+    }
+    
+}
